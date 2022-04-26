307834172a71a749d65f9882598ddad39620a38c
Jorge <jorgesuarez9204@gmail.com>
2022-04-26 00:49:42 -0500

    loope letter

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
-
 const missed = 0;
-for (let i = 0; i < missed.lenght; i++) {}
+
+//for (let i = 0; i < missed.lenght; i++) {}
 
 const overlay = document.getElementById("overlay");
 const btnStartGame = document.querySelector("a");
@@ -21,7 +21,24 @@ const phrases = [
 function getRandomPhraseAsArray(arr) {
   const ramdomNumber = Math.floor(Math.random() * arr.length);
   const ramdomArr = arr[ramdomNumber];
-  const split = ramdomArr.split("");
+  const split = ramdomArr.split(``);
   return split;
 }
+
+const ul = document.querySelector("#phrase ul");
+
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const li = document.createElement(`li`);
+    li.textContent = arr[i];
+    ul.appendChild(li);
+
+    if (arr[i] !== ` `) {
+      li.className = "letter";
+    } else {
+      li.className = "space";
+    }
+  }
+}
 const splitedArray = getRandomPhraseAsArray(phrases);
+const array = addPhraseToDisplay(splitedArray);
